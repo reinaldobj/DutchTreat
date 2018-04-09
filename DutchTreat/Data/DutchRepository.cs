920c7f6f25e3561468ff77a4efdf063bed91323c
Reinaldo Bispo Pereira Junior <reinaldob_j@hotmail.com>
2018-04-08 22:24:29 -0300

    Módulo 8 Finalizado - Web Api

--- DutchTreat/Data/DutchRepository.cs
+++ DutchTreat/Data/DutchRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -17,6 +18,34 @@ namespace DutchTreat.Data
         {
             _ctx = ctx;
             _logger = logger;
+        }
+
+        public IEnumerable<Order> GetAllOrders(bool includeItems)
+        {
+            if (includeItems)
+            {
+                return _ctx
+                    .Orders
+                    .Include(o => o.Items)
+                    .ThenInclude(i => i.Product)
+                    .ToList();
+            }
+            else
+            {
+                return _ctx
+                    .Orders
+                    .ToList();
+            }
+        }
+
+        public Order GetOrderById(int id)
+        {
+            return _ctx
+                .Orders
+                .Include(o => o.Items)
+                .ThenInclude(i => i.Product)
+                .Where(o => o.Id == id)
+                .FirstOrDefault();
         }
 
         public IEnumerable<Product> GetAllProducts()
@@ -63,6 +92,11 @@ namespace DutchTreat.Data
                 _logger.LogError($"Ocorreu um erro ao executar SaveAll: {ex}");
                 return false;
             }
+        }
+
+        public void AddEntity(object model)
+        {
+            _ctx.Add(model);
         }
     }
 }
--- DutchTreat/Data/IDutchRepository.cs
+++ DutchTreat/Data/IDutchRepository.cs
@@ -7,6 +7,11 @@ namespace DutchTreat.Data
     {
         IEnumerable<Product> GetAllProducts();
         IEnumerable<Product> GetByCategory(string category);
+        
+        IEnumerable<Order> GetAllOrders(bool includeItems);
+        Order GetOrderById(int id);
+
         bool SaveAll();
+        void AddEntity(object model);
     }
 }
